44c9c900b0e59c6ecad27ed116537ac4d9d39fde
Pradeep Sahani <mark42virus>
2021-01-19 10:56:35 +0530

    updation

---
+++ javascript1.js
@@ -0,0 +1,17 @@
+// introduction to the javascript. 
+
+// example of javascript. 
+
+function data()
+{
+    document.getElementById("demo").innerHTML = "Hello Pradeep Kumar Sahani"
+}
+
+function bulbOff()
+{
+    document.getElementById("myImage").src = 'image/pic_bulboff.gif';
+}
+
+function bulbOn() {
+    document.getElementById("myImage").src = 'image/pic_bulbon.gif';
+}
